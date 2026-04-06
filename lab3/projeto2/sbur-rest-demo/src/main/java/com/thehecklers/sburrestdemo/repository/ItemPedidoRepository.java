31c426160be561ed62df5df2137b2d4288fe4011
João Santos <98664551+joaosantos13@users.noreply.github.com>
2026-04-06 15:56:21 -0300

    Delete lab3/projeto2/sbur-rest-demo directory

--- lab3/projeto2/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/repository/ItemPedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thehecklers.sburrestdemo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thehecklers.sburrestdemo.model.ItemPedido;
-
-public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
-}
--- lab3/projeto2/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thehecklers.sburrestdemo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thehecklers.sburrestdemo.model.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
